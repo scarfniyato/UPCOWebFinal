dc938e246759d6619c142e4de7902eeb085d3fe9
Syra Mae Looc <123631696+scarfniyato@users.noreply.github.com>
2025-01-24 22:36:13 -0800

    updated

--- client/src/components/PublicPage/PubCharts/LandPollution.jsx
+++ client/src/components/PublicPage/PubCharts/LandPollution.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Navbar from '../navbar2'
-import WaistChart from '../../AdminDashboard/EnviManagement/EnviCharts/waste_charts'
+import React from 'react';
+import Navbar from '../navbar2';
+import WaistChart from '../../AdminDashboard/EnviManagement/EnviCharts/waste_charts';
 import landActive_icon from '../../../assets/landActive_icon.png';
 import water_icon from '../../../assets/water_icon.png';
 import air_icon from '../../../assets/air_icon.png';
@@ -8,39 +8,74 @@ import { Link } from 'react-router-dom';
 
 const LandPollution = () => {
     return (
-        <div>
+        <div style={{ marginTop: '11%', marginBottom: '12%' }}>
             <Navbar />
             <section className='flex flex-col w-full'>
-            <div className='text-center text-dark fbold border-bottom w-5/6'>
-            <h5>Cavite State University - Indang Campus</h5>
-            <p1 className='text-4xl mt-5'>State of the Environment</p1>
-            <div className='text-left mt-20'>
-                State of the Environment / Land Pollution
-            </div>
-            </div>
-            <hr className='border border-dark'/>
-
-            <div className=" ">
-                <div className="img_btn_container flex flex-1 flex-row mt-2 gap-1 w-full ">
-                <Link to="/landpollution" className="active_link">
-                <img src={landActive_icon} alt="Land Pollution" />
-                </Link>
-                <Link to="/airpollution" className="img_btn">
-                <img src={air_icon} alt="Air Pollution" />
-                </Link>
-                <Link to="/waterpollution" className="img_btn">
-                <img src={water_icon} alt="Water Pollution" />
-                </Link>
+                {/* Header Section */}
+                <div className='text-center text-dark fbold border-bottom w-5/6 mx-auto'>
+                    <h5>Cavite State University - Indang Campus</h5>
+                    <p1 className='text-4xl mt-5'>State of the Environment</p1>
+                    <div className='text-left mt-5'>
+                        State of the Environment / Land Pollution
+                    </div>
                 </div>
-            </div>
+                <hr className='border border-dark w-5/6 mx-auto' />
 
+                {/* Icons Section */}
+                <div className="flex justify-center items-center gap-5 my-5">
+                    <Link to="/landpollution" className="active_link">
+                        <img src={landActive_icon} alt="Land Pollution" className="w-12 h-12" />
+                    </Link>
+                    <Link to="/airpollution" className="img_btn">
+                        <img src={air_icon} alt="Air Pollution" className="w-12 h-12" />
+                    </Link>
+                    <Link to="/waterpollution" className="img_btn">
+                        <img src={water_icon} alt="Water Pollution" className="w-12 h-12" />
+                    </Link>
+                </div>
 
-            <div className='bg-white p-5 rounded-xl m-5 w-5/6'>
-            <WaistChart />
-            </div>
+                {/* Chart Section */}
+                <div
+                    className='bg-white p-3 rounded-xl m-5 w-5/6 mx-auto'
+                    style={{
+                        width: '80%', // Explicit width
+                        height: '90vh', // Explicit height
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <div style={{ width: '100%', height: '100%' }}>
+                        <WaistChart
+                            options={{
+                                responsive: true, // Ensure the chart is responsive
+                                maintainAspectRatio: false, // Disable aspect ratio to fit the container
+                                layout: {
+                                    padding: 10, // Add some padding to the chart
+                                },
+                                scales: {
+                                    x: {
+                                        ticks: {
+                                            font: {
+                                                size: 12, // Adjust font size for labels
+                                            },
+                                        },
+                                    },
+                                    y: {
+                                        ticks: {
+                                            font: {
+                                                size: 12, // Adjust font size for labels
+                                            },
+                                        },
+                                    },
+                                },
+                            }}
+                        />
+                    </div>
+                </div>
             </section>
         </div>
-      )
-}
+    );
+};
 
-export default LandPollution
+export default LandPollution;
--- client/src/components/PublicPage/PubCharts/WaterPollution.jsx
+++ client/src/components/PublicPage/PubCharts/WaterPollution.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Navbar from '../navbar2'
-import WaterChart from '../../AdminDashboard/EnviManagement/EnviCharts/water_charts'
+import React from 'react';
+import Navbar from '../navbar2';
+import WaterChart from '../../AdminDashboard/EnviManagement/EnviCharts/water_charts';
 import land_icon from '../../../assets/land_icon.png';
 import waterActive_icon from '../../../assets/waterActive_icon.png';
 import air_icon from '../../../assets/air_icon.png';
@@ -8,39 +8,81 @@ import { Link } from 'react-router-dom';
 
 const WaterPollution = () => {
     return (
-        <div>
+        <div style={{ marginTop: '10%', marginBottom: '8%' }}> {/* Removed marginTop */}
             <Navbar />
             <section className='flex flex-col w-full'>
-            <div className='text-center text-dark fbold border-bottom w-5/6'>
-            <h5>Cavite State University - Indang Campus</h5>
-            <p1 className='text-4xl mt-5'>State of the Environment</p1>
-            <div className='text-left mt-20'>
-                State of the Environment / Air Pollution
-            </div>
-            </div>
-            <hr className='border border-dark'/>
-
-
-            <div className=" ">
-                <div className="img_btn_container flex flex-1 flex-row mt-2 gap-1 w-full ">
-                <Link to="/landpollution" className="img_btn">
-                <img src={land_icon} alt="Land Pollution" />
-                </Link>
-                <Link to="/airpollution" className="img_btn">
-                <img src={air_icon} alt="Air Pollution" />
-                </Link>
-                <Link to="/waterpollution" className="active_link">
-                <img src={waterActive_icon} alt="Water Pollution" />
-                </Link>
+                {/* Header Section */}
+                <div
+                    className='text-center text-dark fbold border-bottom w-5/6 mx-auto'
+                    style={{
+                        marginTop: '0px', // Removed additional space above the header
+                        paddingTop: '0px', // Adjusted padding for the header
+                    }}
+                >
+                    <h5>Cavite State University - Indang Campus</h5>
+                    <p1 className='text-4xl mt-3'>State of the Environment</p1> {/* Adjusted margin */}
+                    <div className='text-left mt-3'>
+                        State of the Environment / Water Pollution
+                    </div>
                 </div>
-            </div>
+                <hr className='border border-dark w-5/6 mx-auto' />
 
-            <div className='bg-white p-5 rounded-xl m-5 w-5/6'>
-            <WaterChart />
-            </div>
+                {/* Icons Section */}
+                <div className="flex justify-center items-center gap-5 my-5">
+                    <Link to="/landpollution" className="img_btn">
+                        <img src={land_icon} alt="Land Pollution" className="w-12 h-12" />
+                    </Link>
+                    <Link to="/airpollution" className="img_btn">
+                        <img src={air_icon} alt="Air Pollution" className="w-12 h-12" />
+                    </Link>
+                    <Link to="/waterpollution" className="active_link">
+                        <img src={waterActive_icon} alt="Water Pollution" className="w-12 h-12" />
+                    </Link>
+                </div>
+
+                {/* Chart Section */}
+                <div
+                    className='bg-white p-4 rounded-lg m-4 w-4/6 mx-auto'
+                    style={{
+                        width: '80%',
+                        height: '100vh',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        paddingBottom: '15%',
+                    }}
+                >
+                    <div style={{ width: '60%', height: '60%' }}>
+                        <WaterChart
+                            options={{
+                                responsive: true, // Ensure the chart is responsive
+                                maintainAspectRatio: false, // Disable aspect ratio to fit the container
+                                layout: {
+                                    padding: 10, // Add some padding to the chart
+                                },
+                                scales: {
+                                    x: {
+                                        ticks: {
+                                            font: {
+                                                size: 12, // Adjust font size for labels
+                                            },
+                                        },
+                                    },
+                                    y: {
+                                        ticks: {
+                                            font: {
+                                                size: 12, // Adjust font size for labels
+                                            },
+                                        },
+                                    },
+                                },
+                            }}
+                        />
+                    </div>
+                </div>
             </section>
         </div>
-      )
-}
+    );
+};
 
-export default WaterPollution
+export default WaterPollution;
